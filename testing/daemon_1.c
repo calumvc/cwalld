3dd4048e2937458cf6d12c40e2a6070508315d6a
calumvc <c@calumv.cc>
2026-02-23 17:50:51 +0000

    daemon tracking and parsing auditd logs for pid and daemon, error handling upgrade, initial setup upon running added

--- testing/daemon_1.c
+++ testing/daemon_1.c
@@ -10,25 +10,31 @@ int main(void){
     f = fopen("/home/cal/testgrounds/static_wall/obj1", "r"); // allowed
     sleep(5);
     fclose(f);
+    sleep(2);
 
     f = fopen("/home/cal/testgrounds/static_wall/obj1", "w"); // allowed
     sleep(5);
     fclose(f);
+    sleep(2);
 
     f = fopen("/home/cal/testgrounds/static_wall/obj2", "r"); // denied
     sleep(5);
     fclose(f);
+    sleep(2);
 
     f = fopen("/home/cal/testgrounds/static_wall/obj2", "w"); // denied
     sleep(5);
     fclose(f);
+    sleep(2);
 
     f = fopen("/home/cal/testgrounds/static_wall/obj3", "r"); // allowed
     sleep(5);
     fclose(f);
+    sleep(2);
 
     f = fopen("/home/cal/testgrounds/static_wall/obj3", "w"); // denied
     sleep(5);
     fclose(f);
+    sleep(2);
   }
 }
