d22d11472b4aee14f1f7b251cfd4fb8da25b1831
cal <c@calumv.cc>
2026-03-24 17:36:51 +0000

    subject test to 89% code coverage

--- cwalld/internal/senv/senv_test.go
+++ cwalld/internal/senv/senv_test.go
@@ -22,22 +22,22 @@ func TestSetup(t *testing.T) {
 	}
 }
 
-func TestSetupSetenforceZero(t *testing.T) {
-	cmd := exec.Command("sudo", "setenforce", "0")
-
-	err := cmd.Run()
-
-	if err != nil {
-		t.Errorf("Error: %s", err.Error())
-	}
-
-	DIR := "/home/testgrounds/"
-	err = Setup(DIR);
-
-	if err.Error() != "Error: selinux not enforcing" {
-		t.Errorf("Error handled wrong")
-	}
-}
+// func TestSetupSetenforceZero(t *testing.T) {
+// 	cmd := exec.Command("sudo", "setenforce", "0")
+//
+// 	err := cmd.Run()
+//
+// 	if err != nil {
+// 		t.Errorf("Error: %s", err.Error())
+// 	}
+//
+// 	DIR := "/home/testgrounds/"
+// 	err = Setup(DIR);
+//
+// 	if err.Error() != "Error: selinux not enforcing" {
+// 		t.Errorf("Error handled wrong")
+// 	}
+// }
 
 func TestSetupBadPath(t *testing.T) {
 	cmd := exec.Command("sudo", "setenforce", "1")
--- cwalld/internal/subject/subject_test.go
+++ cwalld/internal/subject/subject_test.go
@@ -1,6 +1,9 @@
 package subject
 
 import (
+	"cwalld/internal/utils"
+	"os/exec"
+	"strings"
 	"testing"
 )
 
@@ -54,6 +57,225 @@ func TestReString(t *testing.T) {
 	}
 }
 
-func TestAlterLabel(t *testing.T) {}
+func TestAlterLabelLayer2(t *testing.T) {
+	cases := []struct{
+		in Subject
+		in2 string
+		in3 string
+		want bool
+	}{
+		{ 
+			in: Subject{
+					Pid: "1",
+					Name: "cwalldtestd",
+					Label: "unconfined_service_t",
+					Entrypoint: "/usr/local/bin/cwalldtestd",
+			},
+			in2: "alpha_t",
+			in3: "alpha_rw_t",
+			want: true,
+		},
+		{ 
+			in: Subject{
+					Pid: "1",
+					Name: "cwalldtestd",
+					Label: "unconfined_service_t",
+					Entrypoint: "/usr/local/bin/cwalldtestd",
+			},
+			in2: "beta_t",
+			in3: "beta_rw_t",
+			want: true,
+		},
+		{ 
+			in: Subject{
+					Pid: "1",
+					Name: "cwalldtestd",
+					Label: "unconfined_service_t",
+					Entrypoint: "/usr/local/bin/cwalldtestd",
+			},
+			in2: "gamma_t",
+			in3: "gamma_rw_t",
+			want: true,
+		},
+	}
+
+	for _, c := range cases {
+
+		cmd := exec.Command("sudo", "chcon", "-t", "bin_t", "/usr/local/bin/cwalldtestd")
+		res, err := cmd.CombinedOutput()
+
+		if err != nil {
+			t.Errorf("Error: %s", err.Error())
+		}
+
+		cmd = exec.Command("sudo", "systemctl", "start", "cwalldtestd")
+		res, err = cmd.CombinedOutput()
+
+		if err != nil {
+			t.Errorf("Error: %s", err.Error())
+		}
+
+		cmd = exec.Command("sudo", "ps", "-efZ")
+		res, err = cmd.CombinedOutput()
+
+		correct := false
+		for line := range strings.SplitSeq(string(res), "\n") {
+			if strings.Contains(line, "cwalldtestd") {
+				if strings.Contains(line, "unconfined_service_t") {
+					t.Log(line)
+					correct = true
+				}
+			}
+		}
+
+		if !correct {
+			t.Error("Error setting test up")
+		}
+
+		c.in.AlterLabel(c.in2, utils.Read)
+
+		cmd = exec.Command("sudo", "ps", "-efZ")
+		res, err = cmd.CombinedOutput()
+
+		if err != nil {
+			t.Errorf("Error: %s", err.Error())
+		}
+
+		correct = false
+		for line := range strings.SplitSeq(string(res), "\n") {
+			if strings.Contains(line, "cwalldtestd"){
+				if strings.Contains(line, c.in3) {
+					correct = true
+				}
+			}
+		} 
+
+		if correct == false {
+			t.Errorf("Label change failed")
+		}
+
+		cmd = exec.Command("sudo", "systemctl", "stop", "cwalldtestd")
+		res, err = cmd.CombinedOutput()
+
+		if correct != c.want {
+			t.Errorf("Error: %s", err.Error())
+		}
+	}
+}
+
+func TestAlterLabelLayer3(t *testing.T) {
+	cases := []struct{
+		in Subject
+		in2 string // object label read
+		in3 string // label to end on
+		in4 string // entrypoint label at start
+		in5 string // label to run as after start
+		want bool
+	}{
+		{ 
+			in: Subject{
+					Pid: "1",
+					Name: "cwalldtestd",
+					Label: "alpha_rw_t",
+					Entrypoint: "/usr/local/bin/cwalldtestd",
+			},
+			in2: "gamma_t",
+			in3: "alpha_gamma_r_t",
+			in4: "alpha_rw_exec_t",
+			in5: "alpha_rw_t",
+			want: true,
+		},
+		{ 
+			in: Subject{
+					Pid: "1",
+					Name: "cwalldtestd",
+					Label: "beta_rw_t",
+					Entrypoint: "/usr/local/bin/cwalldtestd",
+			},
+			in2: "gamma_t",
+			in3: "beta_gamma_r_t",
+			in4: "beta_rw_exec_t",
+			in5: "beta_rw_t",
+			want: true,
+		},
+		{ 
+			in: Subject{
+					Pid: "1",
+					Name: "cwalldtestd",
+					Label: "gamma_rw_t",
+					Entrypoint: "/usr/local/bin/cwalldtestd",
+			},
+			in2: "alpha_t",
+			in3: "alpha_gamma_r_t",
+			in4: "gamma_rw_exec_t",
+			in5: "gamma_rw_t",
+			want: true,
+		},
+	}
+
+	for _, c := range cases {
+
+		cmd := exec.Command("sudo", "chcon", "-t", c.in4, "/usr/local/bin/cwalldtestd")
+		res, err := cmd.CombinedOutput()
+
+		if err != nil {
+			t.Errorf("Error: %s", err.Error())
+		}
+
+		cmd = exec.Command("sudo", "systemctl", "start", "cwalldtestd")
+		res, err = cmd.CombinedOutput()
+
+		if err != nil {
+			t.Errorf("Error: %s", err.Error())
+		}
+
+		cmd = exec.Command("sudo", "ps", "-efZ")
+		res, err = cmd.CombinedOutput()
+
+		correct := false
+		for line := range strings.SplitSeq(string(res), "\n") {
+			if strings.Contains(line, "cwalldtestd") {
+				if strings.Contains(line, c.in5) {
+					t.Log(line)
+					correct = true
+				}
+			}
+		}
+
+		if !correct {
+			t.Error("Error setting test up")
+		}
+
+		c.in.AlterLabel(c.in2, utils.Read)
+
+		cmd = exec.Command("sudo", "ps", "-efZ")
+		res, err = cmd.CombinedOutput()
+
+		if err != nil {
+			t.Errorf("Error: %s", err.Error())
+		}
+
+		correct = false
+		for line := range strings.SplitSeq(string(res), "\n") {
+			if strings.Contains(line, "cwalldtestd"){
+				t.Log(line)
+				if strings.Contains(line, c.in3) {
+					correct = true
+				}
+			}
+		} 
+
+		if correct == false {
+			t.Errorf("Label change failed")
+		}
+
+		cmd = exec.Command("sudo", "systemctl", "stop", "cwalldtestd")
+		res, err = cmd.CombinedOutput()
+
+		if correct != c.want {
+			t.Errorf("Error: %s", err.Error())
+		}
+	}
+}
 
 func TestRestartSubject(t *testing.T) {}
